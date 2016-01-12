9d44adda3f396243bd79abd294015b041957194f
SngGrozy <guozhi.sun@yolu-inc.com>
2016-01-12 22:28:13 +0800

    新增背景图

--- FMPlayer/Sad/AppDelegate.m
+++ FMPlayer/Sad/AppDelegate.m
@@ -12,7 +12,6 @@
 #import "ViewController.h"
 #import "GZSongListViewController.h"
 #import "UIColor+Theme.h"
-#import "RESideMenu.h"
 
 @interface AppDelegate ()
 
@@ -42,7 +41,7 @@
     RESideMenu *sideMenuViewController = [[RESideMenu alloc] initWithContentViewController:homeNavigationController
                                                                     leftMenuViewController:nil
                                                                    rightMenuViewController:rightMenuViewController];
-    sideMenuViewController.backgroundImage = [UIImage imageNamed:@"Stars"];
+    sideMenuViewController.backgroundImage = [UIImage imageNamed:@"background"];
     sideMenuViewController.menuPreferredStatusBarStyle = 1; // UIStatusBarStyleLightContent
     sideMenuViewController.delegate = self;
     sideMenuViewController.contentViewShadowColor = [UIColor blackColor];
